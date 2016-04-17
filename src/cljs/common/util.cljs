b6cfe1ffa3f848817b0b86ff587dc3a733c1e988
Mitch Comardo <mcomardo@cmn.com>
2016-04-16 19:35:14 -0500

    adding some common, basic utility functions for logging

---
+++ src/cljs/common/util.cljs
@@ -0,0 +1,11 @@
+(ns common.util)
+
+(defn log
+  "Log a Clojure thing."
+  [thing]
+  (js/console.log (pr-str thing)))
+
+(defn js-log
+  "Log a JavaScript thing."
+  [thing]
+  (js/console.log thing))
