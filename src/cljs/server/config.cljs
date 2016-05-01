950fd34d0cbd27a8e1ed58b30df318816448c751
Mitch Comardo <comamitc@gmail.com>
2016-05-01 12:41:17 -0500

    correct config path

--- src/cljs/server/config.cljs
+++ src/cljs/server/config.cljs
@@ -2,4 +2,4 @@
   (:require [cljs.nodejs :as nodejs]))
 
 ;; keep config in javascript since we are mostly passing values to other JS libs
-(def config (nodejs/require "../../application.json"))
+(def config (nodejs/require "../../resources/config/application.json"))
