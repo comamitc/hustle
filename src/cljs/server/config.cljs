cd11ab939a0dab1ac33b62987b6a1ed812975731
Mitch Comardo <comamitc@gmail.com>
2016-05-01 09:33:07 -0500

    config reader

---
+++ src/cljs/server/config.cljs
@@ -0,0 +1,5 @@
+(ns server.config
+  (:require [cljs.nodejs :as nodejs]))
+
+;; keep config in javascript since we are mostly passing values to other JS libs
+(def config (nodejs/require "../../application.json"))
